9a6b336b9c96bdf057ab039d3c604b825c2a6862
Johnatan Santana Soares <johnatan.santana@semantix.com.br>
2020-02-06 14:07:44 -0300

    saving things
    Signed-off-by: Johnatan Santana Soares <johnatan.santana@semantix.com.br>

---
+++ haveFun/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/event", createEvent).Methods("POST")
+	router.HandleFunc("/events", getAllEvents).Methods("GET")
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// -----------------------------------------------------------------------------
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Intro Golang",
+		Description: "Come on, let's go CARALHO",
+	},
+}
+
+// ------------------------------------------------
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Erro tiozão, olha ai a desc ou o titulo")
+	}
+
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
+}
+
+func getOneEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	for _, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+
+	}
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(events)
+}
+
+// func updateEvent(w http.ResponseWriter, r *http.Request) {
+// 	eventID := mux.Vars(r)["id"]
+
+// 	var updateEvent event
+
+// 	reqBody, err := ioutil.ReadAll(r.Body)
+
+// }
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Trying to learning something new
+
+This repository you will only find trash codes and tests.
